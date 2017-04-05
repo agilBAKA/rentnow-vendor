7bfff0d4a7ea936b4e4c1195bc9a04f8993960f1
agilbaka <devi.krisdiansyah@gmail.com>
2017-04-05 17:19:16 +0700

    form vehicle

--- modules/pages/properties/add/form-vehicle.php
+++ modules/pages/properties/add/form-vehicle.php
@@ -1,1 +1,103 @@
-hello this is form
+<form action="" class="default-form">
+    
+    <div class="row">
+         
+        <div class="col-md-3">
+            <div class="field">
+                <label> Jenis kendaraan</label>
+                <select name="" id="">
+                    <option value="" disabled selected>Pilih kendaraan Anda</option>
+                    <option value="">Option 1</option>
+                    <option value="">Option 2</option>
+                    <option value="">Option 3</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="field">
+                <label>Merk</label>
+                <select name="" id="">
+                    <option value="" disabled selected>pilih Merk kendaraan</option>
+                    <option value="">Option 1</option>
+                    <option value="">Option 2</option>
+                    <option value="">Option 3</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="field">
+                <label>Model</label>
+                <select name="" id="">
+                    <option value="" disabled selected>Model kendaraan</option>
+                    <option value="">Option 1</option>
+                    <option value="">Option 2</option>
+                    <option value="">Option 3</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="field field-money">
+                <label>Tarif perjam</label>
+                <a href="" class="link">Ubah jenis Tarif</a>
+                <select name="" id="" class="select-money">
+                    <option value="" disabled selected>IDR</option>
+                    <option value="">USD</option>
+                    <option value="">EUR</option>
+                    <option value="">SGD</option>
+                </select>
+                <input type="text" value="1.000.000">
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="field">
+                <label> Keterangan</label>
+                <textarea name="" id="" cols="30" rows="4" placeholder="tulis keterangan singkat menngenai kendaraan disini..."></textarea>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="field">
+                <label>Warna kendaraan</label>
+                <input type="text" placeholder="Tulis warna sesuai STNK">
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="field">
+                <label> Transmisi</label>
+                <div class="box-radio">
+                    <div class="style-radio">
+                        <input id="manual" name="Transmisi" type="radio">
+                        <label>Manual</label>
+                    </div>
+                    <div class="style-radio">
+                        <input id="otomatis" name="Transmisi" type="radio">
+                        <label>Otomatis</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+    	<div class="col-md-3">
+    		<div class="field">
+    			<label>Nomor Polisi</label>
+    			<input type="text" placeholder="Tulis nomor polisi ">
+    		</div>
+    	</div>
+    </div>
+
+	<div class="action-form">
+        <button class="btn-secondary">Save as draft</button>
+        <button class="btn-primary">Save &amp; publish</button>
+    </div>
+
+</form>
